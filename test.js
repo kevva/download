98544795277acc2894634b8e8fb7ba2738a0f2ba
Kevin Martensson <kevinmartensson@gmail.com>
2013-08-23 19:58:22 +0200

    Add status code test

--- test.js
+++ test.js
@@ -1,19 +1,23 @@
-/*global describe, it, afterEach */
+/*global describe, it */
 'use strict';
 
 var fs = require('fs');
+var assert = require('assert');
 var download = require('./download');
-var path = require('path');
 
 describe('download()', function () {
-    afterEach(function () {
-        fs.unlinkSync(path.join('tmp', 'test.png'));
-        fs.rmdirSync('tmp');
-    });
     it('should download a file', function (cb) {
         download('https://www.google.se/images/srpr/logo4w.png', 'tmp/test.png')
         .on('close', function () {
-            fs.stat('tmp/test.png', cb);
+            fs.statSync('tmp/test.png');
+            cb();
+        });
+    });
+    it('should return status code 404', function (cb) {
+        download('https://github.com/not/existing/url', 'tmp/404')
+        .on('response', function (res) {
+            assert.equal(res.statusCode, '404');
+            cb();
         });
     });
 });
