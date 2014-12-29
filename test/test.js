3ea028d787baeb1819ae7db1ee1d966b2d12f69f
Shinnosuke Watanabe <snnskwtnb@gmail.com>
2014-12-29 23:58:50 +0900

    Update to support multiple-source download
    Current version downloads only a single file specified last.

--- test/test.js
+++ test/test.js
@@ -3,14 +3,21 @@
 var archiveType = require('archive-type');
 var Download = require('../');
 var nock = require('nock');
+var fs = require('vinyl-fs');
 var path = require('path');
 var fixture = path.join.bind(path, __dirname, 'fixtures');
+var rimraf = require('rimraf');
 var tar = require('gulp-tar');
 var test = require('ava');
 
 test('expose a constructor', function (t) {
 	t.plan(1);
 	t.assert(typeof Download === 'function');
+});
+
+test('return an instance if it called without `new`', function (t) {
+	t.plan(1);
+	t.assert(Download() instanceof Download);
 });
 
 test('add a plugin to the middleware stack', function (t) {
@@ -50,6 +57,32 @@ test('download a file', function (t) {
 	});
 });
 
+test('download multiple files', function (t) {
+	t.plan(9);
+
+	var download = new Download()
+		.get('http://foo.com/test-file.zip')
+		.get('http://foo.com/nested/test-file.zip');
+
+	var scope = nock('http://foo.com')
+		.get('/test-file.zip')
+		.replyWithFile(200, fixture('test-file.zip'))
+		.get('/nested/test-file.zip')
+		.replyWithFile(200, fixture('test-file.zip'));
+
+	download.run(function (err, files) {
+		t.assert(!err, err);
+		t.assert(scope.isDone());
+		t.assert(files.length === 2);
+		t.assert(files[0].path === 'test-file.zip');
+		t.assert(files[0].url === 'http://foo.com/test-file.zip');
+		t.assert(archiveType(files[0].contents) === 'zip');
+		t.assert(files[1].path === 'test-file.zip');
+		t.assert(files[1].url === 'http://foo.com/nested/test-file.zip');
+		t.assert(archiveType(files[1].contents) === 'zip');
+	});
+});
+
 test('download a file and rename it', function (t) {
 	t.plan(5);
 
@@ -84,6 +117,61 @@ test('download and extract a file', function (t) {
 		t.assert(!err, err);
 		t.assert(scope.isDone());
 		t.assert(files[0].path === 'file.txt');
+	});
+});
+
+test('download and extract multiple files', function (t) {
+	t.plan(5);
+
+	var download = new Download({ extract: true })
+		.get('http://foo.com/test-file.zip')
+		.get('http://foo.com/nested/test-file.zip');
+
+	var scope = nock('http://foo.com')
+		.get('/test-file.zip')
+		.replyWithFile(200, fixture('test-file.zip'))
+		.get('/nested/test-file.zip')
+		.replyWithFile(200, fixture('test-file.zip'));
+
+	download.run(function (err, files) {
+		t.assert(!err, err);
+		t.assert(scope.isDone());
+		t.assert(files.length === 2);
+		t.assert(files[0].path === 'file.txt');
+		t.assert(files[1].path === 'file.txt');
+	});
+});
+
+test('specify destination folder', function (t) {
+	t.plan(7);
+
+	var dest = path.join(__dirname, 'tmp');
+
+	var download = new Download({ extract: true })
+		.get('http://foo.com/test-file.zip')
+		.get('http://foo.com/test.js')
+		.dest(dest);
+
+	var scope = nock('http://foo.com')
+		.get('/test-file.zip')
+		.replyWithFile(200, fixture('test-file.zip'))
+		.get('/test.js')
+		.replyWithFile(200, __filename);
+
+	download.run(function (err, files) {
+		t.assert(!err, err);
+		t.assert(scope.isDone());
+
+		fs.src(['file.txt', 'test.js'], { cwd: dest })
+			.on('data', function (file) {
+				t.assert(file.path === files.shift().path);
+				t.assert(file.isBuffer());
+			})
+			.on('end', function () {
+				rimraf(dest, function (err) {
+					t.assert(!err, err);
+				});
+			});
 	});
 });
 
@@ -164,5 +252,16 @@ test('error on 404', function (t) {
 		t.assert(err.message === 'Couldn\'t connect to http://foo.com/error (404)');
 		t.assert(err.code === 404);
 		t.assert(scope.isDone());
+	});
+});
+
+test('error on invalid URL', function (t) {
+	t.plan(1);
+
+	var download = new Download()
+		.get('foobar');
+
+	download.run(function (err) {
+		t.assert(err.message === 'Specify a valid URL');
 	});
 });
