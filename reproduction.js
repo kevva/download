76416040f129accb56c74df73a84d7ae1f1e9e70
James Talmage <james@talmage.io>
2016-06-08 02:47:32 -0400

    remove reproduction.js
    oops! that wasn't supposed to make it in there!

--- reproduction.js
+++
@@ -1,6 +0,0 @@
-const download = require('./');
-
-download('https://nodejs.org/dist/v4.4.5/node-v4.4.5-linux-x86.tar.xz')
-	.then(res => console.log(`done ${res.length}`))
-	.catch(e => console.log(e));
-
