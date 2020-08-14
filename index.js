3b68a868e49d6bb636e5b51a2a91e6b0c8426acd
LI JUN <zwlijun@gmail.com>
2020-08-14 12:24:33 +0800

    fix build fail
      index.js:4:8
      ✖   4:8   Unexpected use of require("url").URL. Use the global variable URL instead.  node/prefer-global/url
      ✖  75:45  Prefer await to then().                                                     promise/prefer-await-to-then
      ✖  78:5   Prefer await to then().                                                     promise/prefer-await-to-then
      ✖  93:5   Prefer await to then().                                                     promise/prefer-await-to-then
      ✖  94:5   Prefer await to then().                                                     promise/prefer-await-to-then
      ✖  97:9   Prefer await to then().                                                     promise/prefer-await-to-then
      ✖  97:24  Prefer await to then().                                                     promise/prefer-await-to-then

--- index.js
+++ index.js
@@ -18,84 +18,80 @@ const fsP = pify(fs);
 const filenameFromPath = res => path.basename(new URL(res.requestUrl).pathname);
 
 const getExtFromMime = res => {
-	const header = res.headers['content-type'];
+    const header = res.headers['content-type'];
 
-	if (!header) {
-		return null;
-	}
+    if (!header) {
+        return null;
+    }
 
-	const exts = extName.mime(header);
+    const exts = extName.mime(header);
 
-	if (exts.length !== 1) {
-		return null;
-	}
+    if (exts.length !== 1) {
+        return null;
+    }
 
-	return exts[0].ext;
+    return exts[0].ext;
 };
 
 const getFilename = (res, data) => {
-	const header = res.headers['content-disposition'];
+    const header = res.headers['content-disposition'];
 
-	if (header) {
-		const parsed = contentDisposition.parse(header);
+    if (header) {
+        const parsed = contentDisposition.parse(header);
 
-		if (parsed.parameters && parsed.parameters.filename) {
-			return parsed.parameters.filename;
-		}
-	}
+        if (parsed.parameters && parsed.parameters.filename) {
+            return parsed.parameters.filename;
+        }
+    }
 
-	let filename = filenameFromPath(res);
+    let filename = filenameFromPath(res);
 
-	if (!path.extname(filename)) {
-		const ext = (fileType(data) || {}).ext || getExtFromMime(res);
+    if (!path.extname(filename)) {
+        const ext = (fileType(data) || {}).ext || getExtFromMime(res);
 
-		if (ext) {
-			filename = `${filename}.${ext}`;
-		}
-	}
+        if (ext) {
+            filename = `${filename}.${ext}`;
+        }
+    }
 
-	return filename;
+    return filename;
 };
 
-module.exports = (uri, output, opts) => {
-	if (typeof output === 'object') {
-		opts = output;
-		output = null;
-	}
+module.exports = async function(uri, output, opts) {
+    if (typeof output === 'object') {
+        opts = output;
+        output = null;
+    }
 
-	const strictSSL = Boolean(process.env.npm_config_strict_ssl === 'false' ? '' : process.env.npm_config_strict_ssl);
-	this.opts = {
-		encoding: null,
-		rejectUnauthorized: strictSSL,
-		...opts
-	};
+    const strictSSL = Boolean(process.env.npm_config_strict_ssl === 'false' ? '' : process.env.npm_config_strict_ssl);
+    this.opts = {
+        encoding: null,
+        rejectUnauthorized: strictSSL,
+        ...opts
+    };
 
-	const stream = got.stream(uri, opts);
+    const stream = got.stream(uri, opts);
+    let streamData = null;
 
-	const promise = pEvent(stream, 'response').then(res => {
-		const encoding = opts.encoding === null ? 'buffer' : opts.encoding;
-		return Promise.all([getStream(stream, {encoding}), res]);
-	}).then(result => {
-		const [data, res] = result;
+    const streamResponse = await pEvent(stream, 'response');
+    const encoding = opts.encoding === null ? 'buffer' : opts.encoding;
 
-		if (!output) {
-			return opts.extract && archiveType(data) ? decompress(data, opts) : data;
-		}
+    const result = await Promise.all([getStream(stream, {encoding}), streamResponse]);
+    const [data, res] = result;
 
-		const filename = opts.filename || filenamify(getFilename(res, data));
-		const outputFilepath = path.join(output, filename);
+    if (!output) {
+        streamData = opts.extract && archiveType(data) ? decompress(data, opts) : data;
+    } else {
+        const filename = opts.filename || filenamify(getFilename(res, data));
+        const outputFilepath = path.join(output, filename);
 
-		if (opts.extract && archiveType(data)) {
-			return decompress(data, path.dirname(outputFilepath), opts);
-		}
+        if (opts.extract && archiveType(data)) {
+            streamData = decompress(data, path.dirname(outputFilepath), opts);
+        } else {
+            await makeDir(path.dirname(outputFilepath));
+            streamData = await fsP.writeFile(outputFilepath, data);
+        }
+    }
 
-		return makeDir(path.dirname(outputFilepath))
-			.then(() => fsP.writeFile(outputFilepath, data))
-			.then(() => data);
-	});
-
-	stream.then = promise.then.bind(promise);
-	stream.catch = promise.catch.bind(promise);
-
-	return stream;
+    return streamData;
 };
